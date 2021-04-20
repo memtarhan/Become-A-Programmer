3e5d2b72186d1b3d6471f609fcda50beb636b648
Mehmet Tarhan <hi@memtarhan.com>
2021-04-20 17:16:19 +0300

    [Beyond the Fundamentals] - Iteration

---
+++ 2-beyond-the-fundamentals/iteration.py
@@ -0,0 +1,16 @@
+spices = [
+    "salty",
+    "pepper",
+    "cumin",
+    "turmeric"
+]
+
+for spice in spices:
+    print("Spice:", spice)
+print("Enjoy!")
+
+index = 5 
+while index <= 100:
+    print("Index:", index)
+    index += 5
+
