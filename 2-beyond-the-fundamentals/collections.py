fd020a03d234b8ea0b0e0cd1e9f6f64bdedbc5d4
Mehmet Tarhan <hi@memtarhan.com>
2021-04-20 17:04:43 +0300

    [Beyond the Fundamentals] - Collections

---
+++ 2-beyond-the-fundamentals/collections.py
@@ -0,0 +1,20 @@
+# Lists 
+cities = ["New York", "London", "Istanbul", "Tokyo"]
+# print(type(cities))
+
+# Dictionaries 
+animals = {
+    "bird": ["Parrot", "Owl"],
+    "mammal": ["Lion", "Bison"],
+    "fish": ["Shark"] 
+}
+# print(animals)
+
+# 3rd city 
+city = cities[2]
+print("3rd city is", city)
+
+# 1st mammal 
+mammal = animals["mammal"][0]
+print("1st mammal is", mammal)
+
