b76bff5fed9fc684d44c6e8e5317dd9795efe50b
Mehmet Tarhan <hi@memtarhan.com>
2022-06-06 22:04:31 +0100

    [Test Driven Development] - TDD and Agile

--- 11-test-driven-development/chapter-1/Hangman/src/main/java/Hangman.java
+++ 11-test-driven-development/chapter-1/Hangman/src/main/java/Hangman.java
@@ -1,4 +1,12 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Hangman {
+
+    char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o'};
+    Set<String> usedWordsSet = new HashSet<>();
+
     public int countLetter(String word, char letter) {
         int count = 0;
         for (char character : word.toCharArray()) {
@@ -6,5 +14,21 @@ public class Hangman {
         }
 
         return count;
+    }
+
+    public String fetchWord() {
+        return "pizza";
+    }
+
+    public String fetchWord(int requestedLength) {
+        Random random = new Random();
+        int index = random.nextInt(alphabet.length);
+        String word = String.valueOf(alphabet[index]).repeat(Math.max(0, requestedLength));
+
+        if (usedWordsSet.contains(word)) return fetchWord(requestedLength);
+        else {
+            usedWordsSet.add(word);
+            return word;
+        }
     }
 }
--- 11-test-driven-development/chapter-1/Hangman/src/test/java/HangmanTests.java
+++ 11-test-driven-development/chapter-1/Hangman/src/test/java/HangmanTests.java
@@ -1,5 +1,9 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HangmanTests {
@@ -15,5 +19,40 @@ class HangmanTests {
         assertEquals(count, 1);
         assertTrue(count > 0);
         assertFalse(count > word.length());
+    }
+
+    @Test
+    void testLengthOfFetchedWord() {
+        Hangman hangman = new Hangman();
+        String word = hangman.fetchWord();
+
+        assertEquals(5, word.length());
+    }
+
+    @Test
+    void  testLengthOfFetchedWordRandom() {
+        Random random = new Random();
+        int requestedLength = random.nextInt(6) + 5;
+        Hangman hangman = new Hangman();
+        String word = hangman.fetchWord(requestedLength);
+
+        assertEquals(requestedLength, word.length());
+    }
+
+    @Test
+    void testUniquenessOfFetchedWord() {
+        Random random = new Random();
+        int requestedLength = 0;
+        Set<String> usedWordsSet = new HashSet<>();
+        int round = 0;
+        String word = null;
+        Hangman hangman = new Hangman();
+
+        while (round < 2) {
+            requestedLength = random.nextInt(6) + 5;
+            word = hangman.fetchWord(requestedLength);
+            round++;
+            assertTrue(usedWordsSet.add(word));
+        }
     }
 }
